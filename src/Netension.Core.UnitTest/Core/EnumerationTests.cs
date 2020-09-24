fd127ea1236339bc39a9ff103bcb25c11cf5b028
Bence Mihucza <Mihben@gmail.com>
2020-09-24 21:57:39 +0200

    Implement Enumeration.

---
+++ src/Netension.Core.UnitTest/Core/EnumerationTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Netension.Core.UnitTest.Core
+{
+    public class TestEnumeration : Enumeration
+    {
+        public static TestEnumeration FirstTestEnumeration => new TestEnumeration(1, "TestEnumeration");
+        public static TestEnumeration SecondTestEnumeration => new TestEnumeration(2, "TestEnumeration");
+
+        public TestEnumeration(int id, string name) 
+            : base(id, name)
+        {
+        }
+
+    }
+
+    public class EnumerationTests
+    {
+
+        [Fact]
+        public void Enumeration_Equals_WithSameIds()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.True(TestEnumeration.FirstTestEnumeration.Equals(TestEnumeration.FirstTestEnumeration));
+        }
+
+        [Fact]
+        public void Enumeration_Equals_WithDifferencIds()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.False(TestEnumeration.FirstTestEnumeration.Equals(TestEnumeration.SecondTestEnumeration));
+        }
+    }
+}
---
+++ src/Netension.Core/Enumeration.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Netension.Core
+{
+    /// <summary>
+    /// Base of Enumeration classes.
+    /// </summary>
+    public abstract class Enumeration : IComparable
+    {
+        /// <summary>
+        /// Unique identifier. Two <see cref="Enumeration"/> instance equal by the Id.
+        /// </summary>
+        public int Id { get; }
+        /// <summary>
+        /// Name of the <see cref="Enumeration"/>.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Create a new <see cref="Enumeration"/> instance.
+        /// </summary>
+        /// <param name="id">Unique identifier of the <see cref="Enumeration"/>.</param>
+        /// <param name="name">Name of the <see cref="Enumeration"/>.</param>
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Gets the all value of the <see cref="Enumeration"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of the <see cref="Enumeration"/>.</typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public |
+                                             BindingFlags.Static |
+                                             BindingFlags.DeclaredOnly);
+
+            return fields.Select(f => f.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Enumeration otherValue))
+                return false;
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override string ToString() => Name;
+
+        public int CompareTo(object obj) => Id.CompareTo(((Enumeration)obj).Id);
+
+        public override int GetHashCode()
+        {
+            return 2108858624 + Id.GetHashCode();
+        }
+    }
+}
