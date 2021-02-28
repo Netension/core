bedaf58ae2110b7a665658ac65985f524700bfb9
Bence Mihucza <Mihben@gmail.com>
2021-02-28 19:22:10 +0100

    Fix code smells.

--- src/Netension.Core/ValueObject.cs
+++ src/Netension.Core/ValueObject.cs
@@ -4,17 +4,42 @@ using System.Linq;
 
 namespace Netension.Core
 {
+    /// <summary>
+    /// Base class of value objects.
+    /// </summary>
+    /// <remarks>
+    /// Value objects are immutable object without identity. Every value object is equal by their properties.
+    /// </remarks>
     public abstract class ValueObject
     {
+        /// <summary>
+        /// Equal operator of ValueObject.
+        /// </summary>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right operand.</param>
+        /// <returns>True if two value objects are equal, otherwise false.</returns>
         public static bool operator ==(ValueObject left, ValueObject right)
         {
             return EqualOperator(left, right);
         }
+
+        /// <summary>
+        /// Not equal operator of ValueObject.
+        /// </summary>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right operand.</param>
+        /// <returns>True if two value objects are not equal, otherwise false.</returns>
         public static bool operator !=(ValueObject left, ValueObject right)
         {
             return NotEqualOperator(left, right);
         }
 
+        /// <summary>
+        /// Methods of equal operator.
+        /// </summary>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right operand.</param>
+        /// <returns>True if two value objects are equal, otherwise false.</returns>
         protected static bool EqualOperator(ValueObject left, ValueObject right)
         {
             if (left is null ^ right is null)
@@ -24,6 +49,12 @@ namespace Netension.Core
             return left is null || left.Equals(right);
         }
 
+        /// <summary>
+        /// Methods of not equal operator.
+        /// </summary>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right operand.</param>
+        /// <returns>True if two value objects are not equal, otherwise false.</returns>
         protected static bool NotEqualOperator(ValueObject left, ValueObject right)
         {
             return !(EqualOperator(left, right));
@@ -50,6 +81,10 @@ namespace Netension.Core
                 .Aggregate((x, y) => x ^ y);
         }
 
+        /// <summary>
+        /// Get values for the equals comparing.
+        /// </summary>
+        /// <returns>Values of the properties.</returns>
         protected abstract IEnumerable<object> GetEqualityComponents();
     }
 }
