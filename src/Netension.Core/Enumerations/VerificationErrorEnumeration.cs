b3d9d9331546acdb7d0a0214631db1837b7afd02
Bence, Mihucza <mihben@gmail.com>
2021-04-02 19:25:05 +0200

    Add VerificationException

---
+++ src/Netension.Core/Enumerations/VerificationErrorEnumeration.cs
@@ -0,0 +1,13 @@
+﻿namespace Netension.Core.Enumerations
+{
+    public class VerificationErrorEnumeration : Enumeration
+    {
+        public string Message { get; }
+
+        public VerificationErrorEnumeration(int id, string name, string message) 
+            : base(id, name)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ src/Netension.Core/Exceptions/VerificationException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Netension.Core.Exceptions
+{
+    [Serializable]
+    public class VerificationException : Exception
+    {
+        public int Code { get; }
+
+        public VerificationException(int code, string message)
+            : base(message)
+        {
+            Code = code;
+        }
+
+        public VerificationException()
+        {
+        }
+
+        protected VerificationException(SerializationInfo info, StreamingContext context) 
+            : base(info, context)
+        {
+        }
+
+        public VerificationException(string message) 
+            : base(message)
+        {
+        }
+
+        public VerificationException(string message, Exception innerException) 
+            : base(message, innerException)
+        {
+        }
+    }
+}
