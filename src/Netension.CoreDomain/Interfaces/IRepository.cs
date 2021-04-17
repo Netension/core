bd964e9833ca32290aed2b310dec4b223dc0bc0a
Bence, Mihucza <mihben@gmail.com>
2021-04-17 22:07:57 +0200

    Remove TAggregateRoot from IRepository

--- src/Netension.CoreDomain/Interfaces/IRepository.cs
+++ src/Netension.CoreDomain/Interfaces/IRepository.cs
@@ -4,13 +4,12 @@ using System.Threading.Tasks;
 
 namespace Netension.Core.Domain.Interfaces
 {
-    public interface IRepository<TAggregate, TState>
-        where TAggregate : IMemento<TState>
+    public interface IRepository<TState>
         where TState : class
     {
-        Task<TAggregate> GetByIdAsync(Guid id, CancellationToken cancellationToken);
-        Task InsertAsnyc(TAggregate aggregate, CancellationToken cancellationToken);
-        Task UpdateAsync(TAggregate aggregate, CancellationToken cancellationToken);
+        Task<AggregateRoot<TState>> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task InsertAsnyc(IMemento<TState> aggregate, CancellationToken cancellationToken);
+        Task UpdateAsync(IMemento<TState> aggregate, CancellationToken cancellationToken);
         Task DeleteAsync(Guid id, CancellationToken cancellationToken);
     }
 }
